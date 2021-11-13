fbed37575afc02dea968645b38e93b34e316de16
alexandre-pod <24512899+alexandre-pod@users.noreply.github.com>
2021-11-13 19:09:05 +0100

    Use SDWebImage

--- testSDWebImageApps/ViewController.swift
+++ testSDWebImageApps/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ViewController: UIViewController {
 
+    private let imageView = UIImageView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+
+        imageView.contentMode = .scaleAspectFit
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
 
+        imageView.sd_setImage(with: URL(string: "https://raw.githubusercontent.com/SDWebImage/SDWebImage/master/SDWebImage_logo.png"), completed: nil)
+    }
 }
 
